2207b8dd8c27dbcdd56de67209cb98bfcbc801f2
w0Iff <128382983+w0Iff@users.noreply.github.com>
2024-02-19 01:01:13 +0100

    x
    x

--- testy/test_a_user.py
+++ testy/test_a_user.py
@@ -1,7 +1,7 @@
 import unittest
-from main import app, db, User, Kamera
+from main import app, db, User
 
-class TestFindUserWithCamera(unittest.TestCase):
+class TestCheckUserNamePalindrome(unittest.TestCase):
     def setUp(self):
         app.config['TESTING'] = True
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
@@ -14,30 +14,22 @@ class TestFindUserWithCamera(unittest.TestCase):
             db.session.remove()
             db.drop_all()
 
-    def test_find_user_with_camera(self):
-        user_with_camera = User(name='Jarosław Tylkoniedisco', phone_number='112')
-        user_without_camera = User(name='Adam Malysz', phone_number='997')
+    def test_check_user_name_palindrome(self):
+        data = {'nazwa_osoby': 'Jaroslaw Niedisco'}
+        response = self.app.post('/check_user_name_palindrome', json=data)
 
-        with app.app_context():
-            db.session.add_all([user_with_camera, user_without_camera])
-            db.session.commit()
-
-            camera_for_user = Kamera(user_id=user_with_camera.id)
-            db.session.add(camera_for_user)
-            db.session.commit()
-
-        response = self.app.get('/find_user_with_camera?nazwa_osoby=Jarosław Tylkoniedisco')
         self.assertEqual(response.status_code, 200)
-        data = response.get_json()
-        self.assertIn('Jarosław Tylkoniedisco', data['nazwa_osoby'])
-        self.assertIn('112', data['numer_telefonu'])
-        self.assertTrue(data['ma_kamere'])
+        result = response.get_json()
+        self.assertEqual(result['nazwa_osoby'], 'Jaroslaw Niedisco')
+        self.assertFalse(result['is_palindrome'])
 
-        response_no_camera = self.app.get('/find_user_with_camera?nazwa_osoby=Adam Malysz')
-        data_no_camera = response_no_camera.get_json()
-        self.assertEqual(response_no_camera.status_code, 200)
-        print(data_no_camera['blad'])  # Debugging line
-        self.assertIn('Adam Malysz', data_no_camera['blad'])
+        data_palindrome = {'nazwa_osoby': 'level'}
+        response_palindrome = self.app.post('/check_user_name_palindrome', json=data_palindrome)
+
+        self.assertEqual(response_palindrome.status_code, 200)
+        result_palindrome = response_palindrome.get_json()
+        self.assertEqual(result_palindrome['nazwa_osoby'], 'level')
+        self.assertTrue(result_palindrome['is_palindrome'])
 
 if __name__ == '__main__':
     unittest.main()
